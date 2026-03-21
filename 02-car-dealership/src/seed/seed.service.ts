53124b7a6f30f6a7dca2c6b712a186e14a18aefa
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2026-03-21 18:28:28 -0300

    Fin seccion 5

---
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+import { CarsService } from 'src/cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private carService: CarsService,
+    private brandService: BrandsService
+  ) { }
+
+
+  populateDB() {
+    this.carService.fillCarsWithSeedData(CARS_SEED);
+    this.brandService.fillBrandsWithSeedData(BRANDS_SEED);
+
+    return 'Seed executed';
+  }
+}
